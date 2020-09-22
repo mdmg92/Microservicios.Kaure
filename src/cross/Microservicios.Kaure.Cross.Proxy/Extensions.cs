89d8a20ef425aa99522e835cf8998aa11e5f94a9
Mathias de Mestral <mathiasdemestral@gmail.com>
2020-09-21 21:29:26 -0400

    Agregar proyecto Proxy

---
+++ src/cross/Microservicios.Kaure.Cross.Proxy/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Microservicios.Kaure.Cross.Proxy.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservicios.Kaure.Cross.Proxy
+{
+    public static class Extensions
+    {
+        public static TModel GetOptions<TModel>(this IConfiguration configuration, string section) where TModel : new()
+        {
+            var model = new TModel();
+            configuration.GetSection(section).Bind(model);
+
+            return model;
+        }
+        
+        public static IServiceCollection AddProxyHttp(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpClient, CustomHttpClient>();
+
+            return services;
+        }
+    }
+}
---
+++ src/cross/Microservicios.Kaure.Cross.Proxy/Http/IHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microservicios.Kaure.Cross.Proxy.Http
+{
+    public interface IHttpClient
+    {
+        Task<string> GetStringAsync(string uri);
+
+        Task<HttpResponseMessage> PostAsync<T>(string uri, T item);
+
+        Task<string> GetStringAsync(string uri, string token);
+
+        Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string token);
+    }
+}
